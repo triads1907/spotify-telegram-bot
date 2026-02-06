01e09eb4cbbdbb51c783903d2599e38d6618306d
haker1907 <116299892+haker1907@users.noreply.github.com>
2026-02-06 17:31:32 +0500

    Feature: Add automatic cleanup of old database backups (keep last 2)

--- services/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Модуль сервисов
-"""
-from .spotify_service import SpotifyService
-from .message_builder import MessageBuilder
-from .download_service import DownloadService
-
-__all__ = ['SpotifyService', 'MessageBuilder', 'DownloadService']
