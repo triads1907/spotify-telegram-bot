cb887036cbe792e760928803ccfe72eecd81b615
haker1907 <116299892+haker1907@users.noreply.github.com>
2026-02-10 11:21:15 +0500

    Fix: Add missing import os in db_manager.py

--- database/db_manager.py
+++ database/db_manager.py
@@ -10,6 +10,7 @@ from datetime import datetime, timedelta
 
 from .models import Base, User, Playlist, Track, PlaylistTrack, Album, DownloadHistory, Favorite, TrackCache, AuthToken, TelegramFile, BackupLog
 import config
+import os
 
 
 class DatabaseManager:
